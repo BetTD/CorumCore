16f03cb53cb545d9bb344239a8083512c3d6f1f1
Raúl M <between13131@gmail.com>
2021-07-20 00:29:11 +0200

    Fix: IndexOutOfBounds cuando no se proporcionan argumentos

--- src/main/java/wtf/eugenio/corumcore/commands/CCCommand.java
+++ src/main/java/wtf/eugenio/corumcore/commands/CCCommand.java
@@ -10,28 +10,33 @@ import wtf.eugenio.corumcore.managers.VidasManager;
 public class CCCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (!sender.hasPermission("hg.admin")) {
+        if (!sender.hasPermission("corum.admin")) {
             sender.sendMessage("§cNo puedes ejecutar este comando.");
             return true;
         }
 
-        switch (args[0]) {
-            case "empezardesde0":
-                VidasManager.startFresh();
-                break;
-            case "salvarvida":
-                Bukkit.getScheduler().runTaskAsynchronously(CorumCore.getInstance(), () -> VidasManager.saveLife(args[1]));
-                break;
-            case "endeardia":
-                VidasManager.endDay();
-                break;
-            case "sethourctdwn":
-                VidasManager.cHours = Integer.parseInt(args[1]);
-            case "setminctdwn":
-                VidasManager.cMinutes = Integer.parseInt(args[1]);
-            case "setsecctdwn":
-                VidasManager.cSeconds = Integer.parseInt(args[1]);
+        if (args.length!=0) {
+            switch (args[0]) {
+                case "empezardesde0":
+                    VidasManager.startFresh();
+                    break;
+                case "salvarvida":
+                    Bukkit.getScheduler().runTaskAsynchronously(CorumCore.getInstance(), () -> VidasManager.saveLife(args[1]));
+                    break;
+                case "endeardia":
+                    VidasManager.endDay();
+                    break;
+                case "sethourctdwn":
+                    VidasManager.cHours = Integer.parseInt(args[1]);
+                case "setminctdwn":
+                    VidasManager.cMinutes = Integer.parseInt(args[1]);
+                case "setsecctdwn":
+                    VidasManager.cSeconds = Integer.parseInt(args[1]);
+            }
+        } else {
+            sender.sendMessage("§cUso: /cc <empezardesde0|salvarvida|endeardia|sethourctdwn|setminctdwn|setsecctdwn>");
         }
+
 
         return true;
     }
