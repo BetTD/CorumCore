6006d6dfa5986fff01a61493e0fd4d6f97235985
Raúl M <between13131@gmail.com>
2021-07-22 19:39:30 +0200

    Añadir string de webhook-url

--- src/main/java/wtf/eugenio/corumcore/config/Settings.java
+++ src/main/java/wtf/eugenio/corumcore/config/Settings.java
@@ -1,9 +1,12 @@
 package wtf.eugenio.corumcore.config;
 
 import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
 import wtf.eugenio.corumcore.CorumCore;
 
 public class Settings {
+    private final FileConfiguration config = CorumCore.getInstance().getConfig();
+
     public String joinMessage;
     public int motdLines;
     public String joinMotd;
@@ -11,14 +14,18 @@ public class Settings {
     public String leaveMessage;
     // *------*
     public String challenge;
+    // *------*
+    public String webhookURL;
 
     public void initializeMessages() {
-        joinMessage = ChatColor.translateAlternateColorCodes('&', CorumCore.getInstance().getConfig().getString("joinleave.join-message"));
+        joinMessage = ChatColor.translateAlternateColorCodes('&', config.getString("joinleave.join-message"));
         motdLines = CorumCore.getInstance().getConfig().getInt("joinleave.join-motd-newlines");
-        joinMotd = ChatColor.translateAlternateColorCodes('&', CorumCore.getInstance().getConfig().getString("joinleave.join-motd-message"));
+        joinMotd = ChatColor.translateAlternateColorCodes('&', config.getString("joinleave.join-motd-message"));
         // *------*
-        leaveMessage = ChatColor.translateAlternateColorCodes('&', CorumCore.getInstance().getConfig().getString("joinleave.leave-message"));
+        leaveMessage = ChatColor.translateAlternateColorCodes('&', config.getString("joinleave.leave-message"));
         // *------*
-        challenge = ChatColor.translateAlternateColorCodes('&', CorumCore.getInstance().getConfig().getString("challenge"));
+        challenge = ChatColor.translateAlternateColorCodes('&', config.getString("challenge"));
+        // *------*
+        webhookURL = config.getString("webhook-url");
     }
 }
