4ced841081711cfffc74e2fd705e7cb2c1588f0a
Raúl M <between13131@gmail.com>
2021-07-26 06:37:44 +0200

    Cálculo de tiempo restante separado a su propio método

--- src/main/java/wtf/eugenio/corumcore/managers/CountdownManager.java
+++ src/main/java/wtf/eugenio/corumcore/managers/CountdownManager.java
@@ -8,6 +8,7 @@ import wtf.eugenio.corumcore.CorumCore;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 
 public class CountdownManager {
@@ -17,12 +18,8 @@ public class CountdownManager {
 
     public static void startCosmeticCountdown() {
         taskID = Bukkit.getScheduler().scheduleSyncRepeatingTask(CorumCore.getInstance(), () -> {
-            long diff = countdownlimit.getTime() - System.currentTimeMillis();
-            int s = (int) (diff / 1000) % 60 ;
-            int m = (int) ((diff / (1000*60)) % 60);
-            int h = (int) ((diff / (1000*60*60)) % 24);
-            String msg = "§e§lSiguiente desafío:§f " + String.format("%02d", h) + ":" + String.format("%02d", m) + ":" + String.format("%02d", s);
-            if (diff > 0) sendActionCountdown(msg);
+            Object remainingTime = getRemainingTime(false);
+            if (!remainingTime.equals(false)) sendActionCountdown((String) remainingTime);
             else sendActionCountdown("§e§l¡Desafío terminado!§6 Pronto se anunciará otro desafío.");
         }, 0L, 20L);
     }
@@ -45,6 +42,24 @@ public class CountdownManager {
             return new SimpleDateFormat("dd/MM/yyyy HH:mm").parse(date);
         } catch (java.text.ParseException e) {
             Bukkit.getLogger().log(Level.SEVERE, "Ha ocurrido un error al procesar la fecha \"" + date + "\"\n"+e);
+            return false;
+        }
+    }
+
+    public static Object getRemainingTime(boolean readable) {
+        long diff = countdownlimit.getTime() - System.currentTimeMillis();
+        int s = (int) TimeUnit.MILLISECONDS.toSeconds(diff);
+        int m = (int) TimeUnit.MILLISECONDS.toMinutes(diff);
+        int h = (int) TimeUnit.MILLISECONDS.toHours(diff);
+        int d = (int) TimeUnit.MILLISECONDS.toDays(diff);
+        if (diff > 0) {
+            StringBuilder remainingTime = new StringBuilder();
+            if (d != 0) remainingTime.append(d).append("día(s), ");
+            String format = "%02d:%02d:%02d";
+            if (readable) format = "%02d horas, %02d minutos y %02d segundos";
+            remainingTime.append(String.format(format, h, m, s));
+            return remainingTime.toString();
+        } else {
             return false;
         }
     }
